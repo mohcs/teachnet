c3decddc2c8a93d6b87650b4021bed13e6b24281
Tim <tim.j.strehlow@campus.tu-berlin.de>
2012-11-13 21:38:16 +0100

    minor additions

--- src/main/java/algorithms/TreeElection.java
+++ src/main/java/algorithms/TreeElection.java
@@ -1,17 +1,19 @@
 package algorithms;
 
-import teachnet.algorithm.BasicAlgorithm;
-
 import java.awt.Color;
 import java.util.Vector;
 
+import teachnet.algorithm.BasicAlgorithm;
+
 /**
  * Implementierung des Auswahl-Algorithmus auf Baeumen. Initial werden Knoten
- * weiss dargestellt, nach der Explosionsphase (Nachrichten vom Typ Boolean = 
- * false) werden sie rot, nach der Kontraktionsphase orange angezeigt. Alle 
+ * weiss dargestellt, nach der Explosionsphase (Nachrichten vom Typ Boolean =
+ * false) werden sie rot, nach der Kontraktionsphase orange angezeigt. Alle
  * ueber den Leader informierten Knoten sind am Ende in blau dargestellt.
- *
- * @author Tim Strehlow, Bjoern Stabel, Friedrich Maiwald
+ * 
+ * @author Tim Strehlow 316594, Bjoern Stabel 222128, Friedrich Maiwald 350570
+ *         Gruppe 08
+ * 
  */
 
 public class TreeElection extends BasicAlgorithm {
@@ -30,7 +32,7 @@ public class TreeElection extends BasicAlgorithm {
 		caption = "" + id;
 		color = Color.WHITE;
 	}
-	
+
 	@Override
 	public void initiate() {
 		exploded = true;
@@ -39,7 +41,7 @@ public class TreeElection extends BasicAlgorithm {
 		for (int i = 0; i < checkInterfaces(); i++) {
 			send(i, false);
 		}
-		
+
 	}
 
 	@Override
@@ -50,11 +52,14 @@ public class TreeElection extends BasicAlgorithm {
 				exploded = true;
 				color = Color.RED;
 				for (int i = 0; i < checkInterfaces(); i++) {
-					if (i == interf) continue;
+					if (i == interf) {
+						continue;
+					}
 					send(i, false);
 				}
-			} 
-			// itself as leave detected, send id (end contraction phase for this node)
+			}
+			// itself as leave detected, send id (end contraction phase for this
+			// node)
 			if (checkInterfaces() == 1) {
 				color = Color.ORANGE;
 				caption = id + " (" + max + ")";
@@ -66,13 +71,14 @@ public class TreeElection extends BasicAlgorithm {
 		else if (message instanceof Integer) {
 			if (!contracted) {
 				// gather maximum from children
-				max = Math.max(max, (Integer)message);
+				max = Math.max(max, (Integer) message);
 				contractions.add(interf);
 				// informed by all leaves, send maximum to parent
 				if (contractions.size() == (checkInterfaces() - 1)) {
 					for (int i = 0; i < checkInterfaces(); i++) {
 						if (!contractions.contains(i)) {
-							send(i, Math.max(max, (Integer)message));
+							send(i, Math.max(max, (Integer) message));
+							break;
 						}
 					}
 					// end contraction phase for this node
@@ -84,13 +90,15 @@ public class TreeElection extends BasicAlgorithm {
 			}
 			// information about leader
 			if (contracted) {
-				max = Math.max(max, (Integer)message);
+				max = Math.max(max, (Integer) message);
 				caption = id + " (" + max + ")";
 				color = Color.BLUE;
 				// inform neighbors except source interface
 				for (int i = 0; i < checkInterfaces(); i++) {
-					if (i == interf) continue;
-					send (i, Math.max(max, (Integer)message));
+					if (i == interf) {
+						continue;
+					}
+					send(i, Math.max(max, (Integer) message));
 				}
 
 			}
